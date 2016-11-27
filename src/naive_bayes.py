7c655ffc3d0ac820ec99ec7d86315329f5feb5d5
Dana Mukusheva <d.mukusheva@gmail.com>
2016-11-26 20:25:38 -0800

    Added random picker for comparison

--- src/naive_bayes.py
+++ src/naive_bayes.py
@@ -5,21 +5,22 @@ from sklearn.feature_extraction.text import CountVectorizer
 import cPickle
 import numpy as np
 import random
+from collections import Counter
 data_file = "train_mat_filtered.pkl"
 vocab_file = "vocab_filtered.pkl"
 vocab_inv_file = "vocab_inv_filtered.pkl"
 
 # PARAMETERS
 N_FOLDS = 10 # 10% for test, 90% for train
-
+COMPARE_WITH_RANDOM = True
 METRICS_CHOICE = 'micro'  # computes global precision, recall and f1 (not sample or label wise)
 #TODO: ask which one is better: 'micro' or 'weighted'
 
-#Note: 'micro' causes recall and precision to be the same
+# Note: 'micro' causes recall and precision to be the same
 # http://stats.stackexchange.com/questions/99694/what-does-it-imply-if-accuracy-and-recall-are-the-same
 
 MAX_SAMPLES = 65323 # maximum number of available samples, do not change
-MAX_SAMPLES_TO_USE = 40000
+MAX_SAMPLES_TO_USE = 40000 # can be changed
 assert(MAX_SAMPLES_TO_USE <= MAX_SAMPLES)
 
 
@@ -53,6 +54,18 @@ def get_random_samples(data,labels):
 	indicies = random.sample(xrange(len(data)), MAX_SAMPLES_TO_USE)
 	return data[indicies], labels[indicies]
 
+def evaluate(y_test, y_predicted, results):
+	precision = precision_score(y_test, y_predicted, pos_label=None,average=METRICS_CHOICE) #true positives / (true positives+false positives)
+	recall = recall_score(y_test, y_predicted, pos_label=None,average=METRICS_CHOICE) #true positives /(true positives + false negatives)
+	f1 = f1_score(y_test, y_predicted, pos_label=None,average=METRICS_CHOICE)
+	accuracy = accuracy_score(y_test, y_predicted) #num of correct predictions/ total num of predictions
+	print "accuracy = %.3f, precision = %.3f, recall = %.3f, f1 = %.3f" % (accuracy, precision,recall,f1)
+	results['accuracy'].append(accuracy)
+	results['precision'].append(precision)
+	results['recall'].append(recall)
+	results['f1'].append(f1)
+	return results
+
 def main():
     gnb = MultinomialNB()
 
@@ -60,12 +73,12 @@ def main():
     data = np.array(data)
     labels = np.array(labels)
     data, labels = get_random_samples(data,labels)
-
     print "Loaded ", len(data), " samples and ", len(labels)," labels."
     data = transform_data(data) # get BOW representation
 
     skf = StratifiedKFold(n_splits=N_FOLDS)
-    results = {'accuracy' : 0, 'precision' : 0, 'recall' : 0, 'f1' : 0}
+    results = {'accuracy' : [], 'precision' : [], 'recall' : [], 'f1' : []}
+    results_random = {'accuracy' : [], 'precision' : [], 'recall' : [], 'f1' : []}
     for train, test in skf.split(data, labels):
 		X_train, X_test = data[train], data[test]
 		y_train, y_test = labels[train], labels[test]
@@ -73,19 +86,26 @@ def main():
 		gnb = gnb.fit(X_train, y_train)
 		y_predicted = gnb.predict(X_test)
 
-		precision = precision_score(y_test, y_predicted, pos_label=None,average=METRICS_CHOICE) #true positives / (true positives+false positives)
-		recall = recall_score(y_test, y_predicted, pos_label=None,average=METRICS_CHOICE) #true positives /(true positives + false negatives)
-		f1 = f1_score(y_test, y_predicted, pos_label=None,average=METRICS_CHOICE)
-		accuracy = accuracy_score(y_test, y_predicted) #num of correct predictions/ total num of predictions
-		print "accuracy = %.3f, precision = %.3f, recall = %.3f, f1 = %.3f" % (accuracy, precision,recall,f1)
-		results['accuracy']+=accuracy
-		results['precision']+=precision
-		results['recall']+=recall
-		results['f1']+=f1
+		results = evaluate(y_test, y_predicted, results)
 
-    print "=====================Total average results==================="
+		if COMPARE_WITH_RANDOM:
+			counter = Counter(y_train)
+			prob_weigths = [counter[i]*1./len(y_train) for i in xrange(1,6)]
+			y_predicted = [np.random.choice(xrange(1,6),size=1,replace=False,p=prob_weigths) for i in xrange(len(y_test))]
+			print "-------------Random picker results:------------------"
+			results_random = evaluate(y_test, y_predicted, results_random)
+			print
+
+
+    print "\n=====================Total average results==================="
     for key in results:
-		print key+" = %.3f," % (results[key]/N_FOLDS),
+		print key+" = %.3f," % (sum(results[key])/N_FOLDS),
+
+    if COMPARE_WITH_RANDOM:
+		print
+		print "=====================Total average for random ==================="
+		for key in results:
+			print key+" = %.3f," % (sum(results_random[key])/N_FOLDS),
 
 if __name__ == "__main__":
 	main()
