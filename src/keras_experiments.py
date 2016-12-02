c0fd15735755055541a94c70aef4a46b9f536bde
Dana Mukusheva <d.mukusheva@gmail.com>
2016-12-02 18:11:20 -0500

    Added bidirectional

--- src/keras_experiments.py
+++ src/keras_experiments.py
@@ -4,6 +4,7 @@ from sklearn.preprocessing import LabelEncoder
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import Bidirectional
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 import cPickle
@@ -26,16 +27,16 @@ vocab_inv_file = "vocab_inv_filtered.pkl"
 #PARAMETERS
 N_FOLDS = 10 # 10% for test, 90% for train
 LSTM_MEM_UNITS = 100
-
+BATCH_SIZE = 64
 MAX_SAMPLES = 65323 # maximum number of available samples, do not change
-MAX_SAMPLES_TO_USE = 10000 # can be changed
+MAX_SAMPLES_TO_USE = 5000 # can be changed
 assert(MAX_SAMPLES_TO_USE <= MAX_SAMPLES)
 
 def get_vocabs():
 	with open(vocab_file) as f:
 		vocabulary = cPickle.load(f)
-	for key in vocabulary:
-		vocabulary[key]+=1 # index 0 is reserved for masking
+	# for key in vocabulary:
+	# 	vocabulary[key]+=1 # index 0 is reserved for masking
 
 	with open(vocab_inv_file) as f:
 		vocabulary_inv = cPickle.load(f)
@@ -47,7 +48,7 @@ def load_data():
 		x,y, embedding_matrix = cPickle.load(f)
 	zero_row = np.random.uniform(-.25, .25, 300) #to match 0th index
 	embedding_matrix = np.concatenate((np.array([zero_row]),embedding_matrix))# index 0 is reserved for masking
-	#embedding_matrix = np.concatenate((embedding_matrix, np.zeros((1,300))))
+	
 	for row in xrange(len(x)):
 		for col in xrange(len(x[row])):
 			x[row][col]+=1 #increment stuff by 1 to match the vocab
@@ -79,7 +80,9 @@ def main():
 		y_train, y_test = np.array(labels[train]), np.array(labels[test])
 		X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
 		X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
-		
+		# print max_review_length
+		# print X_train.shape
+		# print X_test.shape
 
 		y_train = encoder.transform(y_train)
 		# convert integers to dummy variables (i.e. one hot encoded)
@@ -91,12 +94,13 @@ def main():
 		model = Sequential()
 		#use existing embedding weights
 		model.add(Embedding(input_dim=vocab_size + 1, output_dim=300, mask_zero=True, weights=[embedding_matrx],input_length=X_train.shape[1])) 
-		model.add(LSTM(LSTM_MEM_UNITS))
+		#model.add(Embedding(input_dim=vocab_size, output_dim=300, mask_zero=True, weights=[embedding_matrx],input_length=X_train.shape[1])) 
+		model.add(Bidirectional(LSTM(LSTM_MEM_UNITS),merge_mode='concat'))
 		#model.add(Dropout(0.5))
 		model.add(Dense(num_labels,activation='sigmoid'))
 		model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 		print(model.summary())
-		model.fit(X_train, y_train, nb_epoch=3, batch_size=64)
+		model.fit(X_train, y_train, nb_epoch=3, batch_size=BATCH_SIZE)
 		# Final evaluation of the model
 		scores = model.evaluate(X_test, y_test, verbose=0)
 		print("Accuracy: %.2f%%" % (scores[1]*100))
